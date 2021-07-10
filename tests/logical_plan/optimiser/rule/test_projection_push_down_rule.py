fa269b51dbd35826d583be64883fa99261d24224
Takao Noguchi <tkngch@runbox.com>
2021-07-10 22:31:37 +0100

    Revise the tests, so that they do not use dataframe

--- tests/logical_plan/optimiser/rule/test_projection_push_down_rule.py
+++ tests/logical_plan/optimiser/rule/test_projection_push_down_rule.py
@@ -1,43 +1,48 @@
 from pathlib import Path
 from uuid import uuid4
 
-from pyquery.data_frame import DataFrame
 from pyquery.datasource.csv_data_source import CsvDataSource
 from pyquery.logical_plan.expression.aggregate.logical_count import LogicalCount
 from pyquery.logical_plan.expression.aggregate.logical_max import LogicalMax
 from pyquery.logical_plan.expression.aggregate.logical_min import LogicalMin
 from pyquery.logical_plan.expression.col import col
 from pyquery.logical_plan.expression.literal.lit import lit
+from pyquery.logical_plan.logical_aggregate import LogicalAggregate
 from pyquery.logical_plan.logical_plan import LogicalPlan
+from pyquery.logical_plan.logical_projection import LogicalProjection
 from pyquery.logical_plan.logical_scan import LogicalScan
+from pyquery.logical_plan.logical_selection import LogicalSelection
 from pyquery.logical_plan.optimiser.rule.projection_push_down_rule import (
     ProjectionPushDownRule,
 )
 
 
 def test(tmp_path: Path):
-    dataframe = _read_csv(tmp_path).project((col("first_name"), col("last_name")))
+    scanned = _scan_csv(tmp_path)
+    projected = LogicalProjection(scanned, (col("first_name"), col("last_name")))
+
     rule = ProjectionPushDownRule()
-    optimised = rule.optimise(dataframe.logical_plan)
+    optimised = rule.optimise(projected)
 
     optimised_plans = _list_plan_strings(optimised)
     assert len(optimised_plans) == 2
     assert optimised_plans[0] == "Projection: #first_name, #last_name"
     assert optimised_plans[1] == "\tScan: employee; [first_name, last_name]"
 
-    original_plans = _list_plan_strings(dataframe.logical_plan)
+    original_plans = _list_plan_strings(projected)
     assert optimised_plans[0] == original_plans[0]
     assert optimised_plans[1] != original_plans[1]
 
 
 def test_with_selection(tmp_path: Path):
-    dataframe = (
-        _read_csv(tmp_path)
-        .filter(col("state").eq(lit("CO")))
-        .project((col("id"), col("first_name"), col("last_name")))
+    scanned = _scan_csv(tmp_path)
+    filtered = LogicalSelection(scanned, col("state").eq(lit("CO")))
+    projected = LogicalProjection(
+        filtered, (col("id"), col("first_name"), col("last_name"))
     )
+
     rule = ProjectionPushDownRule()
-    optimised = rule.optimise(dataframe.logical_plan)
+    optimised = rule.optimise(projected)
 
     optimised_plans = _list_plan_strings(optimised)
     assert len(optimised_plans) == 3
@@ -47,27 +52,29 @@ def test_with_selection(tmp_path: Path):
         optimised_plans[2] == "\t\tScan: employee; [first_name, id, last_name, state]"
     )
 
-    original_plans = _list_plan_strings(dataframe.logical_plan)
+    original_plans = _list_plan_strings(projected)
     assert optimised_plans[0] == original_plans[0]
     assert optimised_plans[1] == original_plans[1]
     assert optimised_plans[2] != original_plans[2]
 
 
 def test_with_selection_and_aggregation(tmp_path: Path):
-    dataframe = (
-        _read_csv(tmp_path)
-        .filter(col("state").eq(lit("CO")).or_(col("state").eq(lit("CA"))))
-        .aggregate(
-            (col("state"),),
-            (
-                LogicalMin(col("salary")),
-                LogicalMax(col("salary")),
-                LogicalCount(col("salary")),
-            ),
-        )
+    scanned = _scan_csv(tmp_path)
+    filtered = LogicalSelection(
+        scanned, col("state").eq(lit("CO")).or_(col("state").eq(lit("CA")))
     )
+    aggregated = LogicalAggregate(
+        filtered,
+        (col("state"),),
+        (
+            LogicalMin(col("salary")),
+            LogicalMax(col("salary")),
+            LogicalCount(col("salary")),
+        ),
+    )
+
     rule = ProjectionPushDownRule()
-    optimised = rule.optimise(dataframe.logical_plan)
+    optimised = rule.optimise(aggregated)
 
     optimised_plans = _list_plan_strings(optimised)
     assert len(optimised_plans) == 3
@@ -78,13 +85,13 @@ def test_with_selection_and_aggregation(tmp_path: Path):
     assert optimised_plans[1] == "\tSelection: #state = 'CO' OR #state = 'CA'"
     assert optimised_plans[2] == "\t\tScan: employee; [salary, state]"
 
-    original_plans = _list_plan_strings(dataframe.logical_plan)
+    original_plans = _list_plan_strings(aggregated)
     assert optimised_plans[0] == original_plans[0]
     assert optimised_plans[1] == original_plans[1]
     assert optimised_plans[2] != original_plans[2]
 
 
-def _read_csv(tmp_dir: Path) -> DataFrame:
+def _scan_csv(tmp_dir: Path) -> LogicalScan:
     filename = tmp_dir.joinpath(f"data-{str(uuid4())}.csv").as_posix()
     with open(filename, "w") as handler:
         handler.write("id,first_name,last_name,state,job_title,salary\n")
@@ -94,8 +101,7 @@ def _read_csv(tmp_dir: Path) -> DataFrame:
         handler.write("4,Von,Mill,,Defensive End,11500\n")
 
     source = CsvDataSource(filename, schema=None, batch_size=1024)
-    scan = LogicalScan("employee", source, ())
-    return DataFrame(scan)
+    return LogicalScan("employee", source, ())
 
 
 def _list_plan_strings(plan: LogicalPlan):
